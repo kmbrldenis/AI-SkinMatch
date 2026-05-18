4929e30b8277c0895813c750cf552fdcb91cfd2b
blkr4bbits <joshua.e.green2023@gmail.com>
2026-05-18 14:25:01 -0400

    properly exported files and started frontend

---
+++ Frontend/AI-skin-match/src/Header.jsx
@@ -0,0 +1,20 @@
+
+
+function Header () {
+    
+    return (
+        <header>
+            <h1>Skin Match</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Take the quiz</li>
+                    <li> Contact us </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
